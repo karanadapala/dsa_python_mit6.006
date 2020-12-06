f060f4fc5d08ac6b562c391eca36e73492d3f630
Karan <karan@Karthiks-MacBook-Pro-2.local>
2020-12-06 15:12:00 -0500

    insertion sort with binary search

--- lecture3.py
+++ lecture3.py
@@ -14,6 +14,38 @@ class Lecture3():
         for i in range(1, len(self.arr)):
             if self.arr[i] < self.arr[i-1]:
                 self.arr[i] = recursion_swap(self, i-1, self.arr[i])
+        # worst case time complexity O(n^2), space Complexity O(n^2)
+        return
+
+    def insertion_sort_Optimized(self):
+        def binary_search(self, arr, low, high, x): 
+            if high >= low: 
+                mid = (high + low) // 2
+                if mid == 0 and x < arr[mid]:
+                    return 0
+
+                elif arr[mid] < x < arr[mid+1]: 
+                    return mid+1
+
+                elif low+1 == high-1 and arr[low] < x < arr[high-1]: 
+                        return high-1
+
+                elif arr[mid] > x: 
+                    return binary_search(self, arr, low, mid - 1, x) 
+        
+                elif arr[mid] < x: 
+                    return binary_search(self, arr, mid + 2, high, x) 
+
+        for i in range(1, len(self.arr)):
+            if self.arr[i] < self.arr[i-1]:
+                temp = self.arr[i]
+                temp_ind = i
+                next_ind = binary_search(self, self.arr, 0, i, temp)
+                while  temp_ind != next_ind:
+                    self.arr[temp_ind] = self.arr[temp_ind-1]
+                    temp_ind -= 1
+                self.arr[next_ind] = temp
+        # worst case time complexity O(n^2), space Complexity O(nlogn)
         return
 
 
@@ -21,3 +53,6 @@ my_list = Lecture3([4,1,3,5,6,2])
 print(my_list.arr)
 my_list.insertion_sort_basic()
 print(my_list.arr)
+my_list2 = Lecture3([4,1,3,5,6,2])
+my_list2.insertion_sort_Optimized()
+print(my_list2.arr)
