52e7a00a24b6b6b3b6e75a049f75be1170106ee3
Karan <karan@Karthiks-MacBook-Pro-2.local>
2020-12-07 20:36:08 -0500

    heapify-done

--- lecture4.py
+++ lecture4.py
@@ -12,3 +12,36 @@ A Heap is an array representation of a binary tree, where if element 'x' of the 
 iii. x's right child is at 2^1+1
 
 '''
+from collections import deque
+
+class Heap():
+    def __init__(self, arr: list):
+        self.heap = self.heapify(arr)
+    '''
+    Objectives:
+    insert
+    extract
+    sort 
+    '''
+    def heapify(self, arr: list):
+        def recursiv_swap(self, ind, arr, num):
+            if ind == 1 or num < arr[(ind)//2-1]:
+                temp = arr[ind-1]
+                arr[ind-1] = num
+                return temp
+            else:
+                temp = arr[ind-1]
+                arr[ind-1] = recursiv_swap(self, ind//2, arr, num)
+                return temp
+
+        for i in range(1, len(arr)+1):
+            if arr[(i//2)-1] < arr[i-1]:
+                arr[i-1] = recursiv_swap(self, i//2, arr, arr[i-1])
+        return arr
+
+    def insert(self, num: int or float):
+        return 
+
+
+ds = Heap([1,2,3,4,5,6,7])
+print(ds.heap) # [7, 4, 6, 1, 3, 2, 5]
